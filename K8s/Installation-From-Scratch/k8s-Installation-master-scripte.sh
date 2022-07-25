e569ef2b90453a6f40f3361599b581abed261b06
Hassan Eid <105084413+Hassan-Eid-Hassan@users.noreply.github.com>
2022-07-25 10:23:09 +0200

    Update k8s-Installation-master-scripte.sh

--- K8s/Installation-From-Scratch/k8s-Installation-master-scripte.sh
+++ K8s/Installation-From-Scratch/k8s-Installation-master-scripte.sh
@@ -80,7 +80,7 @@ echo '#######################################################
       #######################################################'
 echo
 
-yum install -y yum-utils
+yum install -y yum-utils dnf
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 dnf install -y docker-ce
