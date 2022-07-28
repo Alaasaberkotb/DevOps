868801359a47da5f14512d3779f187e325a36657
Hassan Eid <105084413+Hassan-Eid-Hassan@users.noreply.github.com>
2022-07-28 13:27:06 +0200

    Update k8s-Installation-master-scripte.sh

--- K8s/Installation-From-Scratch/k8s-Installation-master-scripte.sh
+++ K8s/Installation-From-Scratch/k8s-Installation-master-scripte.sh
@@ -170,6 +170,7 @@ kubeadm init --pod-network-cidr 192.168.0.0/16
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
 echo
 echo '#######################################################################################
